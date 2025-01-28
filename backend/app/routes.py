5457b65662761452fd578190fd9c67c6ea414c70
Baran Cengiz <baran.cengiz@metu.edu.tr>
2025-01-29 00:48:56 +0300

    Fix image not seen error

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -60,7 +60,7 @@ async def convert_to_dmc_colors_route(request: Request, file: UploadFile = File(
         os.remove(temp_file)
 
 @router.post("/custom-dmc-colors/")
-async def convert_to_custom_dmc_colors_route(file: UploadFile = File(...), dmc_colors: str = Form(...), max_colors: int = Form(...), image_width: int = Form(...), use_grid_filter: bool = Form(...)):
+async def convert_to_custom_dmc_colors_route(request: Request, file: UploadFile = File(...), dmc_colors: str = Form(...), max_colors: int = Form(...), image_width: int = Form(...), use_grid_filter: bool = Form(...)):
     if not file.content_type.startswith("image/"):
         raise HTTPException(status_code=400, detail="Invalid file type. Please upload an image.")
     
@@ -75,9 +75,10 @@ async def convert_to_custom_dmc_colors_route(file: UploadFile = File(...), dmc_c
     print(selected_dmc_colors)
     try:
         dmc_image_path, dmc_codes, hex_values, color_counts = convert_to_dmc(temp_file, selected_dmc_colors, max_colors, image_width, use_grid_filter)
+        base_url = str(request.base_url).rstrip('/')
         return {
             "message": "Image converted to custom DMC colors successfully",
-            "image_url": f"http://192.168.0.30:8000/{dmc_image_path}",
+            "image_url": f"{base_url}/{dmc_image_path}",
             "dmc_codes": dmc_codes,
             "hex_values": hex_values,
             "color_counts": color_counts
